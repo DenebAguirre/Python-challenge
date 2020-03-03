8165f4e20a01ce2e4e5846b7a4bb2b19b88497b9
DenebAguirre <58702022+DenebAguirre@users.noreply.github.com>
2020-03-03 15:57:47 -0600

    Avances de la tarea

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,6 +8,17 @@ with open(budget_data) as budgetfile:
     reader = csv.reader(budgetfile)
     header = next(reader)
     data = [row for row in reader]
+    totalmonths = len(data)
+    print(totalmonths)
+    proflos = []
+    suma = 0
+    for row in data:
+        proflos.append(int(row[1]))
+
+    for i in proflos:
+        suma = suma + i
     
-    print(header)
-    print(data[0])
+    print(suma)
+
+  
+
